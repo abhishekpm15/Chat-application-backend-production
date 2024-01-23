c77b3cbe1ce641734d07d23bdc6514b1f7abb773
abhishek <pabhishekm@gmail.com>
2024-01-23 13:12:15 +0530

    commit check

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const bodyparser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const http = require('http')
-// const { Server } = require("socket.io");
 
-// const server = http.createServer(app);
-// const io = new Server(server, { cors:{origin: "http://localhost:3000" , methods :["GET", "POST"]} });
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
@@ -15,13 +11,6 @@ app.use(express.json());
 const path = require("path");
 require("dotenv").config();
 
-
-// io.on("connection", (socket)=>{
-//   console.log("user connected " , socket.id)
-//   socket.on("disconnect",()=>{
-//     console.log("disconnected");
-//   })
-// })
  
 mongoose
   .connect(
